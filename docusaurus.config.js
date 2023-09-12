21025db33683d0512552088ef9c8291532a94caa
Haï~ <hai-vr@users.noreply.github.com>
2023-09-12 14:25:40 +0200

    remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,12 +49,14 @@ const config = {
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
+        blog: false
+            // {
+          // showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // }
+        ,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -139,10 +141,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/hai-vr/',
